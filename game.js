55dcd3a0b425bc93a6e7a05f2ec09aab1da9cfd6
Claire Wang <basketballclaire@gmai.lcom>
2014-07-16 13:10:50 -0700

    added files

--- game.js
+++ game.js
@@ -1,3 +1,38 @@
 if(jQuery){
+	var checkAnswers = function(){
+		var answerString = "";
+		var answers = $(":checked");
+			answers.each(function(i) {
+				answerString = answerString + answers[i].value;
+			});
+			$(":checked").each(function(i) {
+				var answerString = answerString + answers[i].value;
+			});
+			checkIfCorrect(answerString);
+		};
+
+		var checkIfCorrect = function9theString){
+	if(parseInt(theString, 16) === 1184569762430638){
+		$("body").addClass("correct");
+		$("h1").text("You Win!");
+		$("canvas").show();
+	}
+};
+	
 	$("#question1").show();
 };
+if(impress){
+	$("#question2").show();
+}
+if(atom){
+	$("#question3").show();
+}
+if(createjs){
+	$("#question4").show();
+}
+if(me){
+	$("#question5").show();
+}
+if(require){
+	$("#question6").show();
+}
